8ed986be3c3bfc501767599e6aec223150112b0e
Robert <fireganavi@gmail.com>
2026-06-26 02:54:46 -0400

    Add Maven test configuration and unit tests

---
+++ src/test/java/com/cen4802/FibonacciAppTest.java
@@ -0,0 +1,31 @@
+package com.cen4802;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciAppTest {
+
+    @Test
+    void testFibonacciBaseCases() {
+        assertEquals(0, FibonacciApp.fibonacci(0));
+        assertEquals(1, FibonacciApp.fibonacci(1));
+    }
+
+    @Test
+    void testFibonacciTenthTerm() {
+        assertEquals(55, FibonacciApp.fibonacci(10));
+    }
+
+    @Test
+    void testFibonacciAnotherValue() {
+        assertEquals(21, FibonacciApp.fibonacci(8));
+    }
+
+
+    @Test
+    void testFibonacciTwelfthTerm() {
+        assertEquals(144, FibonacciApp.fibonacci(12));
+    }
+
+}
