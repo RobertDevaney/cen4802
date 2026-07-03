6bea717833a53cce8058aa17d72d3d9083eb802d
Robert <fireganavi@gmail.com>
2026-07-03 05:08:06 -0400

    Update output for dry run workflow demo

--- src/main/java/com/cen4802/FibonacciApp.java
+++ src/main/java/com/cen4802/FibonacciApp.java
@@ -6,7 +6,7 @@ public class FibonacciApp {
         int term = 10;
         int result = fibonacci(term);
 
-        System.out.println("Docker test rebuild, the 10th term of the Fibonacci sequence is " + result + ".");
+        System.out.println("Dry run workflow demo, the 10th term of the Fibonacci sequence is " + result + ".");
     }
 
     /**
